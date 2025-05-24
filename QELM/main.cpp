fe8bc0232bba11cd1087edfc4ca226d778e765e9
evans-prince <princeevans2004@gmail.com>
2025-05-24 10:14:03 +0530

    Initial Commit

---
+++ QELM/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  QELM
+//
+//  Created by PRINCE  on 5/24/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
