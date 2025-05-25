7e7c9ca3c8b376c32b6892b7273c61411615f8b5
evans-prince <princeevans2004@gmail.com>
2025-05-25 11:02:37 +0530

    Code in working condition. Added functionality of multiple outputs and labeling variables from pla input file.

--- QELM/src/main.cpp
+++ QELM/src/main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <iterator>
+#include <set>
 
 #include "term.hpp"
 #include "quine.hpp"
@@ -10,7 +12,9 @@
 using namespace std;
 
 // Helper to parse PLA format
-bool parsePLA(const string& filename, int& numVars, vector<Term>& minterms, vector<Term>& dontCares) {
+bool parsePLA(const string& filename, int& numVars, int& numOutputs,
+              vector<vector<Term>>& allMinterms, vector<vector<Term>>& allDontCares,
+              vector<string>& inputLabels, vector<string>& outputLabels) {
     ifstream fin(filename);
     if (!fin) {
         cerr << "Error opening input file\n";
@@ -19,14 +23,23 @@ bool parsePLA(const string& filename, int& numVars, vector<Term>& minterms, vect
 
     string line;
     numVars = 0;
-    minterms.clear();
-    dontCares.clear();
+    numOutputs = 1; // default single output
 
     while (getline(fin, line)) {
-        if (line.empty() || line[0] == '#') continue; // skip comments & empty lines
+        if (line.empty() || line[0] == '#') continue; // skip comments
 
         if (line.substr(0, 3) == ".i ") {
             numVars = stoi(line.substr(3));
+        } else if (line.substr(0, 3) == ".o ") {
+            numOutputs = stoi(line.substr(3));
+            allMinterms.resize(numOutputs);
+            allDontCares.resize(numOutputs);
+        } else if (line.substr(0, 4) == ".ilb") {
+            istringstream ss(line.substr(5));
+            inputLabels = {istream_iterator<string>(ss), {}};
+        } else if (line.substr(0, 3) == ".ob") {
+            istringstream ss(line.substr(4));
+            outputLabels = {istream_iterator<string>(ss), {}};
         } else if (line.substr(0, 2) == ".e") {
             break; // end of PLA
         } else if (line[0] == '0' || line[0] == '1' || line[0] == '-') {
@@ -34,20 +47,20 @@ bool parsePLA(const string& filename, int& numVars, vector<Term>& minterms, vect
             string inputBits, outputBits;
             iss >> inputBits >> outputBits;
 
-            // Convert binary string to decimal
             int decimal = 0;
             for (char c : inputBits) {
                 decimal <<= 1;
                 if (c == '1') decimal |= 1;
             }
-
             set<int> covered = {decimal};
-            Term t(inputBits, covered, outputBits == "-");
 
-            if (outputBits == "1") {
-                minterms.push_back(t);
-            } else if (outputBits == "-") {
-                dontCares.push_back(t);
+            for (int i = 0; i < outputBits.size(); ++i) {
+                Term t(inputBits, covered, outputBits[i] == '-');
+                if (outputBits[i] == '1') {
+                    allMinterms[i].push_back(t);
+                } else if (outputBits[i] == '-') {
+                    allDontCares[i].push_back(t);
+                }
             }
         }
     }
@@ -56,19 +69,15 @@ bool parsePLA(const string& filename, int& numVars, vector<Term>& minterms, vect
     return true;
 }
 
-
-
-
-
 int main() {
     const string inputFile = "./data/input.txt";
     const string outputFile = "./data/output.txt";
 
-    int numVars;
-    vector<Term> minterms;
-    vector<Term> dontCares;
+    int numVars, numOutputs;
+    vector<vector<Term>> allMinterms, allDontCares;
+    vector<string> inputLabels, outputLabels;
 
-    if (!parsePLA(inputFile, numVars, minterms, dontCares)) {
+    if (!parsePLA(inputFile, numVars, numOutputs, allMinterms, allDontCares, inputLabels, outputLabels)) {
         cerr << "Failed to parse PLA input\n";
         return 1;
     }
@@ -83,9 +92,12 @@ int main() {
         fout << "Too many variables (" << numVars << ") to run Quine-McCluskey minimization.\n";
         cout << "Variables > 10, skipping minimization.\n";
     } else {
-        vector<Term> essentialPIs = runQuine(minterms, dontCares);
-        string sop = termsToSOP(essentialPIs, numVars);
-        fout << sop << "\n";
+        for (int i = 0; i < numOutputs; ++i) {
+            fout << "# Output function " << (outputLabels.empty() ? to_string(i) : outputLabels[i]) << "\n";
+            vector<Term> essentialPIs = runQuine(allMinterms[i], allDontCares[i]);
+            string sop = termsToSOP(essentialPIs, numVars);
+            fout << sop << "\n\n";
+        }
         cout << "Minimization done! Check output.txt\n";
     }
 
