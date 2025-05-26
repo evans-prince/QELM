4cd38915044a3f3007e10aa8761b30dda3132528
evans-prince <princeevans2004@gmail.com>
2025-05-26 11:15:09 +0530

    added espresso logic

--- QELM/include/espresso.hpp
+++ QELM/include/espresso.hpp
@@ -1,13 +1,30 @@
-//
-//  espresso.hpp
-//  QELM
-//
-//  Created by PRINCE  on 5/24/25.
-//
 
 #ifndef espresso_hpp
 #define espresso_hpp
 
 #include <stdio.h>
+
+#include <vector>
+#include "term.hpp"
+
+using namespace std;
+
+// Structure to represent a PLA table (input-output pairs)
+struct PLACube {
+    Term term;
+    string outputBits;
+
+    PLACube(const Term& t, const string& out)
+        : term(t), outputBits(out) {}
+};
+
+// Reads PLA format and returns cubes for each output function
+vector<vector<PLACube>> groupByOutput(const vector<PLACube>& cubes, int numOutputs);
+
+// Core Espresso function: takes grouped cubes and returns minimized result
+vector<vector<Term>> runEspresso(const vector<vector<PLACube>>& outputGroupedCubes, int numVars);
+
+// Utility function to convert minimized cubes to SOP string
+vector<string> espressoTermsToSOP(const vector<vector<Term>>& result, int numVars);
 
 #endif /* espresso_hpp */
